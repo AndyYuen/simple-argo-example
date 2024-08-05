4facc60973b51194cbaadb4c9cf748158f23e8b7
Andy Yuen <andyyuen105@gmail.com>
2024-08-05 12:18:16 +1000

    Corrected processMysqlYaml.sh error and re-generated mysql-init.yaml.

--- scripts/processMysqlYaml.sh
+++ scripts/processMysqlYaml.sh
@@ -6,13 +6,14 @@ DIR=/tmp/tmp-$(uuidgen | cut -d "-" -f5)-gitops
 # setup tmp working directory
 mkdir $DIR
 cd $DIR
-echo "initSql: https://raw.githubusercontent.com/andyyuen/kafka-sizing/master/src/main/resources/schema-mysql.sql" > values.yaml
+echo "schemaUrl: https://raw.githubusercontent.com/andyyuen/kafka-sizing/master/src/main/resources/schema-mysql.sql" > values.yaml
 
 # Create temporary helm chart for templating the mysql.yaml file
 mkdir templates
 cp $SCRIPT_DIR/../helm-kafka-sizing/templates/mysql.yaml templates/
 cp $SCRIPT_DIR/../helm-kafka-sizing/templates/_mysql-hook.yaml templates/
 cp $SCRIPT_DIR/../helm-kafka-sizing/Chart.yaml .
+cp $SCRIPT_DIR/../helm-kafka-sizing/values.yaml .
 
 # Execute helm chart and save to specified file
 helm template . --values values.yaml
